52d30b6b229080bcbb94e21910db429d6ca519a4
20055568 <lohithbadam10@gmail.com>
2025-03-08 19:14:36 +0000

    Update models.py

--- DataBase/accounts/models.py
+++ DataBase/accounts/models.py
@@ -7,5 +7,5 @@ class User(AbstractUser):
     phone = models.CharField(max_length=15, blank=True)
         address = models.TextField(blank=True)
 
-    def __str__(self):
+ def __str__(self):
         return self.username
