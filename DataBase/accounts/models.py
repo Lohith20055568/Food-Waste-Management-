8440336b4f21841ee78d511f53de1440e67e7fc8
20055568 <lohithbadam10@gmail.com>
2025-03-08 19:06:31 +0000

    Update models.py

--- DataBase/accounts/models.py
+++ DataBase/accounts/models.py
@@ -5,7 +5,7 @@ class User(AbstractUser):
     is_customer = models.BooleanField(default=True)
     is_restaurant = models.BooleanField(default=False)
     phone = models.CharField(max_length=15, blank=True)
-    address = models.TextField(blank=True)
+        address = models.TextField(blank=True)
 
     def __str__(self):
         return self.username
