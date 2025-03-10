4d446144663dd33bedd211e0cb7a044dbc898806
20055568 <lohithbadam10@gmail.com>
2025-03-10 18:21:59 +0000

    Update models.py

--- DataBase/food/models.py
+++ DataBase/food/models.py
@@ -6,7 +6,7 @@ class Category(models.Model):
 
 class FoodItem(models.Model):
     name = models.CharField(max_length=255)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField()
