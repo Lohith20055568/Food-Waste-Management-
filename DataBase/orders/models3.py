d21b1a0cc90df60568dd605c37a6d7328b5b67b3
20055568 <lohithbadam10@gmail.com>
2025-03-23 22:58:40 +0000

    Delete DataBase directory

--- DataBase/orders/models3.py
+++
@@ -1,8 +0,0 @@
-class OrderItem(models.Model):
-    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name="items")
-    food = models.ForeignKey(FoodItem, on_delete=models.CASCADE)
-    quantity = models.PositiveIntegerField(default=1)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-
-    def __str__(self):
-        return f"{self.food.name} x {self.quantity}"
